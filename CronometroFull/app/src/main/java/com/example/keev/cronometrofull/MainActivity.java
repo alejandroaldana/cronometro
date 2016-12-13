58d04cae26d72f06cca7a334f1f8d025cfd0c204
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2016-12-13 18:10:18 +0100

    Desarrollo

--- CronometroFull/app/src/main/java/com/example/keev/cronometrofull/MainActivity.java
+++ CronometroFull/app/src/main/java/com/example/keev/cronometrofull/MainActivity.java
@@ -1,13 +1,76 @@
 package com.example.keev.cronometrofull;
 
+import android.content.Context;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton btnStart, btnPause, btnRestart;
+    TextView txtReloj;
+    Handler customHandler = new Handler();
+    LinearLayout container;
+    Runnable updateTimerThread = new Runnable() {
+        @Override
+        public void run() {
+            timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+            updateTime = timeSwapBuff + timeInMilliseconds;
+            int secs = (int) (updateTime/1000);
+            int mins = (secs/60);
+            secs &= 60;
+            int milliseconds = (int) (updateTime%1000);
+            txtReloj.setText("" + mins + ":" + String.format("%2d", secs) + ":" + String.format("%3d", milliseconds));
+            customHandler.postDelayed(this, 0);
+        }
+    };
+
+    long startTime = 0L,
+         timeInMilliseconds = 0L,
+         timeSwapBuff = 0L,
+         updateTime = 0L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnStart = (ImageButton) findViewById(R.id.btnStart);
+        btnPause = (ImageButton) findViewById(R.id.btnPause);
+        btnRestart = (ImageButton) findViewById(R.id.btnRestart);
+        txtReloj = (TextView) findViewById(R.id.reloj);
+        container = (LinearLayout) findViewById(R.id.container);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startTime = SystemClock.uptimeMillis();
+                customHandler.postDelayed(updateTimerThread, 0);
+            }
+        });
+
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timeSwapBuff += timeInMilliseconds;
+                customHandler.removeCallbacks(updateTimerThread);
+            }
+        });
+
+        btnRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = (LayoutInflater) getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View addView = inflater.inflate(R.layout.row, null);
+                TextView txtValue = (TextView) addView.findViewById(R.id.txtContent);
+                txtValue.setText(txtReloj.getText());
+                container.addView(addView);
+            }
+        });
     }
 }
