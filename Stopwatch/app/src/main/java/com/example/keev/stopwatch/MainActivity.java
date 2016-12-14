9a2791ec3020ffcebc7d49bfe78640a3baff16cb
alejandroaldana <caspandro@gmail.com>
2016-12-14 12:57:08 +0100

    General

--- Stopwatch/app/src/main/java/com/example/keev/stopwatch/MainActivity.java
+++ Stopwatch/app/src/main/java/com/example/keev/stopwatch/MainActivity.java
@@ -1,5 +1,3 @@
-package com.example.keev.stopwatch;
-
 import android.content.Context;
 import android.os.Handler;
 import android.os.SystemClock;
@@ -25,6 +23,33 @@ public class MainActivity extends AppCompatActivity {
             timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
             updateTime = timeSwapBuff + timeInMilliseconds;
             int secs = (int) (updateTime/1000);
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    /* Definir Botones y TextView */
+    Button btnStart, btnPause, btnLap, btnRestart;
+    TextView txtReloj;
+    /**/
+
+    Handler customHandler = new Handler();
+    LinearLayout container;
+    Runnable updateTimerThread = new Runnable() {
+        @Override
+        public void run() {
+            timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+            updateTime = timeSwapBuff + timeInMilliseconds;
+            int secs = (int) (updateTime/1000);
             int mins = (secs/60);
             int milliseconds = (int) (updateTime%1000);
             txtReloj.setText("" + mins + ":" + String.format("%02d", secs) + ":" + String.format("%03d", milliseconds));
@@ -32,10 +57,10 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    long    startTime = 0L,
-            timeInMilliseconds = 0L,
-            timeSwapBuff = 0L,
-            updateTime = 0L;
+    long startTime = 0L,
+         timeInMilliseconds = 0L,
+         timeSwapBuff = 0L,
+         updateTime = 0L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +68,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         /* Inicializar Botones y textView */
-
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnPause = (Button) findViewById(R.id.btnPause);
+        btnLap = (Button) findViewById(R.id.btnLap);
+        btnRestart = (Button) findViewById(R.id.btnRestart);
+        txtReloj = (TextView) findViewById(R.id.reloj);
         /**/
 
         container = (LinearLayout) findViewById(R.id.container);
@@ -53,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 /* Poner el tiempo inicial */
-
+                startTime = SystemClock.uptimeMillis();
                 /**/
 
                 customHandler.postDelayed(updateTimerThread, 0);
@@ -65,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 /* Poner el tiempo de pausa */
-
+                timeSwapBuff += timeInMilliseconds;
                 /**/
 
                 customHandler.removeCallbacks(updateTimerThread);
@@ -80,10 +109,27 @@ public class MainActivity extends AppCompatActivity {
                 TextView txtValue = (TextView) addView.findViewById(R.id.txtContent);
 
                 /* Poner texto */
-
+                txtValue.setText(txtReloj.getText());
                 /**/
 
                 container.addView(addView);
+            }
+        });
+
+        btnRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customHandler.removeCallbacks(updateTimerThread);
+
+                /* Poner valores a cero */
+                startTime = 0L;
+                timeInMilliseconds = 0L;
+                timeSwapBuff = 0L;
+                updateTime = 0L;
+                /**/
+
+                txtReloj.setText("0:00:000");
+                container.removeAllViews();
             }
         });
     }
